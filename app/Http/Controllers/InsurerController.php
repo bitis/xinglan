a5d490aa6ac8e24e8c3cf13e13e5e102a68bb135
Liuyuan <liuyuans@live.com>
2023-07-22 17:46:02 +0800

    保险公司列表

--- app/Http/Controllers/InsurerController.php
+++ app/Http/Controllers/InsurerController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\JsonResponse;
 
 class InsurerController extends Controller
 {
+    /**
+     * 保险公司列表
+     *
+     * @return JsonResponse
+     */
     public function index(): JsonResponse
     {
         return success(Insurer::all());
