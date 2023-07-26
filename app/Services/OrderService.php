ac93fbf71d1f424cc57d272bdb1d2ddd6c0ab033
Liuyuan <liuyuans@live.com>
2023-07-26 17:26:21 +0800

    工单筛选

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -72,7 +72,7 @@ class OrderService
                 $query->where('post_time', '>', $post_time_start);
             })
             ->when($params->get('post_time_end'), function ($query, $post_time_end) {
-                $query->where('post_time', '<=', $post_time_end);
+                $query->where('post_time', '<=', $post_time_end . ' 23:59:59');
             })
             ->when($params->get('insurance_type'), function ($query, $insurance_type) {
                 $query->where('insurance_type', $insurance_type);
