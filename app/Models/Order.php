40cd82b2ddf5f677f7896bf66b3e29f59c7368e7
Liuyuan <liuyuans@live.com>
2023-07-28 15:31:11 +0800

    auto opening bid

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Jobs\BidOpeningJob;
 use App\Jobs\OrderDispatch;
 use App\Models\Enumerations\CheckStatus;
 use App\Models\Traits\DefaultDatetimeFormat;
@@ -143,6 +144,7 @@ class Order extends Model
     {
         static::created(function ($order) {
             if ($order->bid_type != Order::BID_TYPE_JINGJIA) OrderDispatch::dispatch($order);
+            if ($order->bid_type == Order::BID_TYPE_JINGJIA) BidOpeningJob::dispatch($order)->delay($order->bid_end_time);
         });
     }
 
