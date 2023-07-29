9ebf5c9b288ffaaf3453cfe1d2e6da39c0363c42
Liuyuan <liuyuans@live.com>
2023-07-29 16:36:23 +0800

    派遣物损查勘人员

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -269,7 +269,8 @@ class OrderController extends Controller
         try {
             throw_if(!$order = Order::find($request->input('order_id')), '工单未找到');
 
-            throw_if($user->company_id != $order->check_wusun_company_id or $user->company_id != $order->wusun_company_id, '非本公司订单');
+            throw_if($user->company_id != $order->check_wusun_company_id
+                and $user->company_id != $order->wusun_company_id, '非本公司订单');
 
             throw_if($order->wusun_check_accept_at, '查勘已完成');
 
