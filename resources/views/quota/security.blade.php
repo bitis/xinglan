9016b507142360f9cb015b943e4cd54dafd82761
Liuyuan <liuyuans@live.com>
2023-08-02 16:11:07 +0800

    security code verify

--- resources/views/quota/security.blade.php
+++ resources/views/quota/security.blade.php
@@ -3,9 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>show-pdf</title>
+    <title>防伪码核验</title>
 </head>
 <body style="padding:0;margin:0;">
-    <embed src="{{ config('filesystems.disks.qcloud.url') . $quotation->pdf }}" type="application/pdf" width="100%" style="height: 100vh;"/>
+    <iframe
+        style="
+          width:100vw;
+          min-height: 100vh;
+          border: none;
+          overflow: hidden;
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          z-index: 9999;
+        "
+        src="{{ config('filesystems.disks.qcloud.url') . $quotation->pdf }}"
+        frameborder="0"
+      ></iframe>
 </body>
 </html>
