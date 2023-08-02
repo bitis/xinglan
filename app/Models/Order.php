6e92ac5a88e2598527102946aaf333a5bc8cdf5d
Liuyuan <liuyuans@live.com>
2023-08-02 11:06:02 +0800

    延时开标

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,10 +11,11 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    use HasFactory, DefaultDatetimeFormat;
+    use HasFactory, DefaultDatetimeFormat, SoftDeletes;
 
     protected $fillable = [
         'order_number',
