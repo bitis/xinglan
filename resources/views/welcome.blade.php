cb4eb60f155e61576206887a71e873792c7f734b
Liuyuan <liuyuans@live.com>
2023-08-07 16:38:50 +0800

    index page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,66 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
+<head>
+    <title>摘珏铭科技</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 20px;
+        }
+
+        h1 {
+            margin: 0;
+        }
+
+        nav {
+            background-color: #f4f4f4;
+            padding: 10px;
+        }
+
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        nav ul li {
+            display: inline;
+            margin-right: 10px;
+        }
+
+        section {
+            padding: 20px;
+        }
+
+        footer {
+            background-color: #333;
+            color: #fff;
+            padding: 10px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>摘珏铭科技</h1>
+    </header>
+    <nav>
+
+    </nav>
+    <section>
+        <h2>欢迎访问网页</h2>
+        <p>摘珏铭科技</p>
+    </section>
+    <footer>
+		<a href="https://beian.miit.gov.cn/" style="color: #fff;" target="_blank">备案号：</a>
+		<a href="https://beian.miit.gov.cn/"  style="color: #fff;" target="_blank">沪ICP备2023020075号-1</a>
+    </footer>
+</body>
 </html>
