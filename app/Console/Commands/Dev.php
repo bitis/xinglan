4fcb64d97aabef42781717e9e2910ba498c3630b
Liuyuan <liuyuans@live.com>
2023-08-28 10:14:52 +0800

    users filter

--- app/Console/Commands/Dev.php
+++ app/Console/Commands/Dev.php
@@ -2,15 +2,12 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\ApprovalNotifyJob;
 use App\Jobs\QuotaBillPdfJob;
-use App\Jobs\QuotaHistory;
-use App\Models\Company;
-use App\Models\Order;
+use App\Models\ApprovalOrderProcess;
 use App\Models\OrderQuotation;
-use App\Models\Role;
-use App\Models\User;
 use Illuminate\Console\Command;
-use Illuminate\Support\Str;
+use JPush\Client;
 
 
 class Dev extends Command
@@ -32,8 +29,41 @@ class Dev extends Command
     /**
      * Execute the console command.
      */
-    public function handle(): void
+    public function handle(Client $client): void
     {
-       QuotaHistory::dispatch(OrderQuotation::find(13));
+        $process = ApprovalOrderProcess::find(333);
+
+        $extras = [
+            'type' => 'approval',
+            'order_id' => $process->order_id,
+            'process_id' => $process->id,
+            'creator_name' => $process->creator_name,
+        ];
+
+        $android = [
+            'title' => '您有新的审批项待处理',
+            'sound' => 'sound',
+            'alert_type' => 1,
+            'extras' => $extras,
+        ];
+
+        $ios = [
+            'sound' => 'sound',
+            'extras' => $extras
+        ];
+
+//            if ($extras['event'] == 'resetBadge') {
+//                unset($ios['sound']);
+//                unset($android['alert_type']);
+//                $ios['badge'] = 0;
+//            }
+
+        $client->push()
+            ->setPlatform('all')
+            ->addRegistrationId('13065ffa4f1089c0a78')
+            ->androidNotification('您有新的审批项待处理', $android)
+            ->iosNotification('您有新的审批项待处理', $ios)
+            ->setOptions(5,)
+            ->send();
     }
 }
