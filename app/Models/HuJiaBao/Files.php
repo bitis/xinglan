7a6c87263b50c65f493f2e3c2e74dd64bce447c2
Liuyuan <liuyuans@live.com>
2023-08-30 15:29:34 +0800

    上传文件

--- app/Models/HuJiaBao/Files.php
+++ app/Models/HuJiaBao/Files.php
@@ -2,10 +2,31 @@
 
 namespace App\Models\HuJiaBao;
 
+use App\Models\Traits\DefaultDatetimeFormat;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Files extends Model
 {
-    use HasFactory;
+    use HasFactory, DefaultDatetimeFormat;
+
+    protected $fillable = [
+        '@pk',
+        '@type',
+        'AttachFileId',
+        'AttachType',
+        'BusinessObjectId',
+        'DisplayName',
+        'DmsDocId',
+        'FileExt',
+        'FileSize',
+        'IsDeleted',
+        'IsImage',
+        'OrgFileName',
+        'Path',
+        'Sort',
+        'UploadDate',
+        'UploadUserId',
+        'url',
+    ];
 }
