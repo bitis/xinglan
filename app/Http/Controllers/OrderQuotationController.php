b60c30dac2f197530bd9c8dc48225ca6bbb9ab27
Liuyuan <liuyuans@live.com>
2023-12-14 17:43:59 +0800

    对外报价审批

--- app/Http/Controllers/OrderQuotationController.php
+++ app/Http/Controllers/OrderQuotationController.php
@@ -258,11 +258,11 @@ class OrderQuotationController extends Controller
                                 'creator_name' => $user->name,
                                 'order_id' => $order->id,
                                 'company_id' => $option->company_id,
-                                'step' => empty($checkers) ? Approver::STEP_CHECKER : Approver::STEP_REVIEWER,
+                                'step' => Approver::STEP_REVIEWER,
                                 'approval_status' => ApprovalStatus::Pending->value,
                                 'mode' => $option->review_mode,
                                 'approval_type' => $option->type,
-                                'hidden' => !empty($checkers),
+                                'hidden' => true,
                             ];
                             $reviewer_text .= $reviewer['name'] . ', ';
                         }
@@ -281,11 +281,11 @@ class OrderQuotationController extends Controller
                                     'creator_name' => $user->name,
                                     'order_id' => $order->id,
                                     'company_id' => User::find($approvalExtend['user_id'])?->company_id,
-                                    'step' => Approver::STEP_REVIEWER,
+                                    'step' => empty($checkers) ? Approver::STEP_CHECKER : Approver::STEP_REVIEWER,
                                     'approval_status' => ApprovalStatus::Pending->value,
                                     'mode' => $option->review_mode,
                                     'approval_type' => $option->type,
-                                    'hidden' => true,
+                                    'hidden' => !empty($checker),
                                 ];
                             }
                         }
