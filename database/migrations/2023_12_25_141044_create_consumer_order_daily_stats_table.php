718277d835ce0c5871477277f242b0a1136fcd6d
Liuyuan <liuyuans@live.com>
2023-12-25 14:56:47 +0800

    客户案件数量统计

--- database/migrations/2023_12_25_141044_create_consumer_order_daily_stats_table.php
+++ database/migrations/2023_12_25_141044_create_consumer_order_daily_stats_table.php
@@ -15,9 +15,12 @@ return new class extends Migration
             $table->id();
             $table->integer('company_id')->comment('物损公司ID');
             $table->integer('insurance_company_id')->comment('保险公司ID');
-            $table->integer('order_count')->comment('订单数');
-            $table->integer('order_repair_count')->comment('维修订单数');
-            $table->integer('order_mediate_count')->comment('协调订单数');
+            $table->integer('order_count')->default(0)->comment('订单数');
+            $table->integer('order_repair_count')->default(0)->comment('维修订单数');
+            $table->integer('order_mediate_count')->default(0)->comment('协调订单数');
+            $table->date('date');
+            $table->softDeletes();
+            $table->index(['company_id', 'date']);
             $table->timestamps();
         });
     }
