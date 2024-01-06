c906214babd4869a583ec3dfb53a85db6de51587
Liuyuan <liuyuans@live.com>
2024-01-06 18:30:08 +0800

    客户单量

--- app/Console/Commands/InitStats.php
+++ app/Console/Commands/InitStats.php
@@ -31,6 +31,37 @@ class InitStats extends Command
      */
     public function handle()
     {
+        $orders = Order::without('lossPersons')->select('id', 'wusun_company_id', 'insurance_company_id', 'post_time', 'plan_type')->get();
+
+        $bar = $this->output->createProgressBar(count($orders));
+        $bar->start();
+        foreach ($orders as $order) {
+            $bar->advance();
+            if ($order->wusun_company_id) {
+
+                $company = Company::find($order->wusun_company_id);
+
+                $stats_update = [];
+
+                if ($order->plan_type == Order::PLAN_TYPE_REPAIR)
+                    $stats_update = ['order_repair_count' => DB::raw('order_repair_count + 1')];
+                elseif ($order->plan_type == Order::PLAN_TYPE_MEDIATE)
+                    $stats_update = ['order_mediate_count' => DB::raw('order_mediate_count + 1')];
+
+                ConsumerOrderDailyStats::updateOrCreate([
+                    'company_id' => $company->id,
+                    'date' => substr($order->post_time, 0, 10),
+                    'insurance_company_id' => $order->insurance_company_id
+                ], array_merge($stats_update, [
+                    'order_count' => DB::raw('order_count - 1')
+                ]));
+            }
+        }
+        $bar->finish();
+    }
+
+    public function orderStats()
+    {
         $orders = Order::without('lossPersons')->select('id', 'wusun_company_id', 'post_time', 'plan_type')->get();
 
         foreach ($orders as $order) {
